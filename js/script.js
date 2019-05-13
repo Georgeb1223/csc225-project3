a49f332933c95b36addf8a352e7a60aa8ec249e1
George Bauman <georgeb1223@gmail.com>
2019-05-13 16:07:49 -0400

    second commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$('#inch').on("submit",function(e){
+    e.preventDefault();
+    var inches = $('#inputIn').val();
+    axios.get("http://statenweb.mockable.io/conversions/")
+        .then(function(response){
+            var conversion = response.data.centimetersInInch;
+            var ans = conversion * inches;
+            $('#inchAns').html(ans);
+        })
+})
+$('#centimeter').on("submit",function(e){
+    e.preventDefault();
+    var centimeter = $('#inputCm').val();
+    axios.get("http://statenweb.mockable.io/conversions/")
+        .then(function(response){
+            var conversion = response.data.inchesInCm;
+            var ans = conversion * centimeter;
+            $('#centiAns').html(ans);
+        })
+})
